bc35168c8ae21525b629fa3107ffece9140bc3b6
Marcus  Pianco <pianco@MacBook-Pro-de-Marcus.local>
2019-05-10 00:29:59 -0300

    Updates

---
+++ precache-manifest.321bf6ec7387fa7a217823fa98a56209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f78fc0430825abfbc8343593e9851a",
+    "url": "/contact-manager.io/index.html"
+  },
+  {
+    "revision": "766209c10f08b1e4a5ec",
+    "url": "/contact-manager.io/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a27b7c5c64b8a54d583a",
+    "url": "/contact-manager.io/static/css/main.9ac88995.chunk.css"
+  },
+  {
+    "revision": "766209c10f08b1e4a5ec",
+    "url": "/contact-manager.io/static/js/2.5cfc59ec.chunk.js"
+  },
+  {
+    "revision": "a27b7c5c64b8a54d583a",
+    "url": "/contact-manager.io/static/js/main.48fac859.chunk.js"
+  },
+  {
+    "revision": "96920825c4b91fab2a3e",
+    "url": "/contact-manager.io/static/js/runtime~main.d1b63048.js"
+  }
+]);
